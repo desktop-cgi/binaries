fccee65a303285c9b899f7e6c5ab2328671e4e01
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-12-11 10:30:33 +0530

    [desktop-cgi] [cgi-js] [bug] [improvement] Rectifying pathing issues and Adding deps.

--- demo/demo-usage/demos.shell.bat.js
+++ demo/demo-usage/demos.shell.bat.js
@@ -15,3 +15,20 @@
 /* eslint no-console: 0 */
 
 'use strict';
+/**
+ * 
+ * Package: 
+ * Author: Desktop-CGI, Ganesh B
+ * Email: desktopcgi@gmail.com
+ * Description: Nodejs 
+ * Install: 
+ * Github: https://github.com/
+ * npmjs Link: 
+ * File: .js
+ * File Description: 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
--- demo/demo-usage/process.js
+++ demo/demo-usage/process.js
@@ -1,2 +1,19 @@
 
 
+/**
+ * 
+ * Package: 
+ * Author: Desktop-CGI, Ganesh B
+ * Email: desktopcgi@gmail.com
+ * Description: Nodejs 
+ * Install: 
+ * Github: https://github.com/
+ * npmjs Link: 
+ * File: .js
+ * File Description: 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
