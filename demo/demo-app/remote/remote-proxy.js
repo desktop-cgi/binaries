01f305d3246104f599d9e6153a4c4457d9f2ac2c
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-12-11 11:12:43 +0530

    [desktop-cgi] [binaries] Adding bash scripts to binaries for cgi-js. removing www folder amd demos folder

--- demo/demo-app/remote/remote-proxy.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-module.exports = (port) => {
-    var remoteproxy = express();
-    remoteproxy.use("/sub", function (req, res, next) { res.status(200).send("Path //sub"); });
-    remoteproxy.use("/", function (req, res, next) { res.status(200).send("Path //"); });
-    let srv = remoteproxy.listen(port);
-    return { proxy: remoteproxy, server: srv };
-};
